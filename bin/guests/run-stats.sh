6c6291475fc0ee65effe013f2fb0f70aa494a932
Marcin Bugaj <mm.bugaj@gmail.com>
2026-04-30 12:45:53 +0200

    `run-stats.sh` script

---
+++ bin/guests/run-stats.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+ZISK_BIN="/projects/EF/zisk-repos/zisk/target/release"
+
+output=$(PATH="$ZISK_BIN:$PATH" ziskemu "$@" -X 2>&1)
+rc=$?
+
+if [ $rc -ne 0 ]; then
+    printf '%s\n' "$output" >&2
+    exit $rc
+fi
+
+steps=$(printf '%s\n' "$output" | grep -E '^STEPS[[:space:]]' | grep -oE '[0-9,]+' | tr -d ',')
+total=$(printf '%s\n' "$output" | grep -E '^TOTAL[[:space:]]' | grep -oE '[0-9,]+' | head -1 | tr -d ',')
+
+printf '{"steps": %s, "total": %s}\n' "$steps" "$total"
